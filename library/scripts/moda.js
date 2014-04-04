95b388615a54f9248ece5a15332feb70b4977569
Elif Bayrasli <elif.bayrasli@mediahive.com>
2014-04-03 20:41:15 -0400

    Start moda.js project

---
+++ library/scripts/moda.js
@@ -0,0 +1,24 @@
+/*
+*
+* Author: Elif Bayrasli
+* Website: http://www.elifbayrasi.com
+* Date: 04/04/2014
+* Repo: http://github.com/eatsleepweb/modajs
+* Issues: http://github.com/eatsleepweb/modajs/issues
+*
+*/
+
+(function($){
+
+    $.fn.moda = function(options){
+
+        var settings = $.extend({
+
+            overlay: true,
+            responsive: true
+
+        }, options);
+
+    };
+
+}(jQuery));
